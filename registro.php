ecac664b7137b0f66cdef87b39d47e6108fb1944
rodrigolard <roviedo@santisimosalvador.edu.pe>
2022-10-23 16:23:23 -0500

    16.23

--- registro.php
+++ registro.php
@@ -1,13 +1,13 @@
 <?php
-$nombre = $_POST['Nombre'];
+$nombre = $_POST['Nombres'];
 $correo = $_POST['Correo'];
 $telefono = $_POST['Telefono'];
 $asunto = $_POST['Asunto'];
 $mensaje = $_POST['Mensaje'];
-$fecha = date('d/m/Y', time());
 
-$con = mysqli_connect("localhost","tu_usuario_de_basededatos_de_localhost",
-" tu_contraseña_de_basededatos_de_localhost","tu_basededatos_de_localhost");
+
+/*$con = mysqli_connect("localhost","tu_usuario_de_basededatos_de_localhost",
+" tu_contraseña_de_basededatos_de_localhost","tu_basededatos_de_localhost");*/
 
 $header = 'From: ' . $correo . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -17,7 +17,7 @@ $header .= "Content-Type: text/plain";
 $message = "Este mensaje fue enviado por: " . $nombre . " \r\n";
 $message .= "Su e-mail es: " . $correo . " \r\n";
 $message .= "Teléfono de contacto: " . $telefono . " \r\n";
-$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Mensaje: " . $_POST['Mensaje'] . " \r\n";
 $message .= "Enviado el: " . date('d/m/Y', time());
 
 $para = 'roviedo@santisimosalvador.edu.pe';
